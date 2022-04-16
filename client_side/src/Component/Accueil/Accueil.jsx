51468b62808db9728fa487c7d0467d5e854eb643
Isràa hamdine <hi_hamdine@esi.dz>
2022-04-16 23:32:16 +0100

    loading component added

--- client_side/src/Component/Accueil/Accueil.jsx
+++ client_side/src/Component/Accueil/Accueil.jsx
@@ -13,21 +13,19 @@ import MenuItem from '@mui/material/MenuItem';
 import MenuList from '@mui/material/MenuList';
 import './Accueil.css'
 import Sidebar from '../Sidebar/Sidebar'
+import Result from '../Result/Result'
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Brightness1RoundedIcon from '@mui/icons-material/Brightness1Rounded';
 import CircularProgress from '@mui/material/CircularProgress';
 
 const Accueil = () => {
+  
   const [text, setText] = useState("");
   const options = ['Parenté sémantique entre les phrases', 'Parenté sémantique entre les paragraphes', 'Parenté sémantique entre les phrases et les paragraphes', 'Richesse lexicale', 'Richesse lexicale et parenté sémantique'];
   const [open, setOpen] = React.useState(false);
   const anchorRef = React.useRef(null);
   const [selectedIndex, setSelectedIndex] = React.useState(0);
   const [isLoading, setLoading] = useState(null)
+
   const handleSubmit = (event) => {
     setLoading(true)
     event.preventDefault();
@@ -71,50 +69,23 @@ const Accueil = () => {
     setOpen(false);
   };
   const [scoreResult, setScore] = useState();
-  function BasicCard({ score }) {
-    let val
-    if (score >= 2) {
-      val = <Typography variant="h6" component="div" color="#079615">
-        Score de cohérence : {score}
-      </Typography>
+  
+  function RenderResult({isLoading}) {
+    if(isLoading === null) {
+      return <Result hidden={true}/>
     }
-    else if (score >= 1) {
-      val = <Typography variant="h6" component="div" color="#FF9A02">
-        Score de cohérence : {score}
-      </Typography>
-    }
-    else if (score >= 0) {
-      val = <Typography variant="h6" component="div" color="#E33A3A">
-        Score de cohérence : {score}
-      </Typography>
+    else if(isLoading === true) {
+      return (
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '4%' }}>
+        <CircularProgress/>
+        <Result hidden={true}/>
+        </Box>
+          )
     }
     else {
-      val = <Typography variant="h6" component="div">
-        Score de coherence : {score}
-      </Typography>
+      return <Result hidden={false} scoreResult={scoreResult} />
     }
-    return (
-      <div className='result'>
-        <Card sx={{ minWidth: 275, border: 1 }}>
-          <CardContent>
-            <Typography variant="h6" component="div">
-              {val}
-            </Typography>
-          </CardContent>
-          <CardActions sx={{ position: 'relative' }}>
-            <Button size="small">Voir plus</Button>
-          </CardActions>
-        </Card>
-      </div>
-    );
-  }
-  function Score({ scoreResult }) {
-    switch (scoreResult) {
-      case null:
-        return <BasicCard score="" />
-      default:
-        return <BasicCard score={scoreResult} />
-    }
+      
   }
 
   return (
@@ -188,24 +159,7 @@ const Accueil = () => {
             </div>
             <Button type="submit" id='eval_btn'>Évaluer</Button>
           </div>
-          { isLoading == true ? <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '4%' }}><CircularProgress /></Box> :
-          <div id='evalSection'>
-            <Score scoreResult={scoreResult} />
-            <div className='cards-key'>
-              <div className='card1'>
-                <div><Brightness1RoundedIcon sx={{ color: "#079615" }} /></div>
-                <p id='scoreCard'>2 - 3 (élevé)</p>
-              </div>
-              <div className='card1'>
-                <div><Brightness1RoundedIcon sx={{ color: "#FF9A02" }} /></div>
-                <p id='scoreCard'>1 - 2 (moyen)</p>
-              </div>
-              <div className='card1'>
-                <div><Brightness1RoundedIcon sx={{ color: "#E33A3A" }} /></div>
-                <p id='scoreCard'>0 - 1 (bas)</p>
-              </div>
-            </div>
-          </div> }
+          <RenderResult isLoading={isLoading}/>
         </Form>
       </div>
     </>
---
+++ client_side/src/Component/Result/Result.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Brightness1RoundedIcon from '@mui/icons-material/Brightness1Rounded';
+import Button from '@mui/material/Button';
+
+const Result = ({hidden, scoreResult}) => {
+
+    function Score({ scoreResult }) {
+        switch (scoreResult) {
+            case null:
+            return <BasicCard score="" />
+            default:
+            return <BasicCard score={scoreResult} />
+        }
+    }
+
+    function BasicCard({ score }) {
+        let val
+        if (score >= 2) {
+          val = <Typography variant="h6" component="div" color="#079615">
+            Score de cohérence : {score}
+          </Typography>
+        }
+        else if (score >= 1) {
+          val = <Typography variant="h6" component="div" color="#FF9A02">
+            Score de cohérence : {score}
+          </Typography>
+        }
+        else if (score >= 0) {
+          val = <Typography variant="h6" component="div" color="#E33A3A">
+            Score de cohérence : {score}
+          </Typography>
+        }
+        else {
+          val = <Typography variant="h6" component="div">
+            Score de coherence : {score}
+          </Typography>
+        }
+        return (
+          <div className='result'>
+            <Card sx={{ minWidth: 275, border: 1 }}>
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  {val}
+                </Typography>
+              </CardContent>
+              <CardActions sx={{ position: 'relative' }}>
+                <Button size="small">Voir plus</Button>
+              </CardActions>
+            </Card>
+          </div>
+        );
+    }
+
+  return (
+    <div id='evalSection' hidden={hidden}>
+        <Score scoreResult={scoreResult} />
+        <div className='cards-key'>
+        <div className='card1'>
+            <div><Brightness1RoundedIcon sx={{ color: "#079615" }} /></div>
+            <p id='scoreCard'>2 - 3 (élevé)</p>
+        </div>
+        <div className='card1'>
+            <div><Brightness1RoundedIcon sx={{ color: "#FF9A02" }} /></div>
+            <p id='scoreCard'>1 - 2 (moyen)</p>
+        </div>
+        <div className='card1'>
+            <div><Brightness1RoundedIcon sx={{ color: "#E33A3A" }} /></div>
+            <p id='scoreCard'>0 - 1 (bas)</p>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Result
