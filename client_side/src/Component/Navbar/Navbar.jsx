74043d33dac97131d39f9587877ff6c079678959
Isràa hamdine <hi_hamdine@esi.dz>
2022-04-16 21:44:27 +0100

    updated

--- client_side/src/Component/Navbar/Navbar.jsx
+++ client_side/src/Component/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import coherencia from '../../assets/coherencia.png'
 
 
 const Menu = () => (
     <>
-        <div className='menu_div'> < p className='link' > <a href='#accueil' > Accueil </a></p > </div>
-        <div className='menu_div'> < p  > <a href='#apropos' > À propos </a></p > </div>
-        <div className='menu_div'> < p > <a href='#contact' > Contactez nous </a></p> </div>
+        <div className='menu_div'> < p className='link' > <a href='accueil' > Accueil </a></p > </div>
+        <div className='menu_div'> < p  > <a href='apropos' > À propos </a></p > </div>
+        <div className='menu_div'> < p > <a href='contact' > Contactez nous </a></p> </div>
     </>
 )
 const Navbar = () => {
@@ -15,7 +16,7 @@ const Navbar = () => {
         <div id='navbar' >
             <div className='navbar_links' >
                 < div className='navbar_logo' >
-                    <img src={logo}
+                    <img src={coherencia}
                         alt="logo" />
                     La coherencia
                 </div>
